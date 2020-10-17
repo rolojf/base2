7b87890c443df30f58c902fdbc92778ae60a1ea3
rolojf <rolo@tutamail.com>
2020-10-17 00:22:54 -0500

    Update fonts postcss y tailwind files

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,9 +1,34 @@
 // tailwind.config.js
 const defaultTheme = require("tailwindcss/defaultTheme");
+//const colorBrand = 'var(--color-pretty)';
+// Utils
+const round = (num) =>
+   num
+      .toFixed(7)
+      .replace(/(\.[0-9]+?)0+$/, "$1")
+      .replace(/\.0$/, "");
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
+const px = (px) => `${px}px`;
 
 module.exports = {
+   important: true, // See https://tailwindcss.com/docs/configuration#important
+   experimental: {
+      // See https://github.com/tailwindlabs/tailwindcss/pull/2159
+      applyComplexClasses: true,
+   },
    purge: {
-      enabled: false,
+      enabled: process.env.HUGO_ENVIRONMENT === "production",
+      content: ["./hugo_stats.json"],
+      mode: "all",
+      options: {
+         //whitelist: [ 'pl-1', 'pl-3' ],
+         defaultExtractor: (content) => {
+            let els = JSON.parse(content).htmlElements;
+            els = els.tags.concat(els.classes, els.ids);
+            return els;
+         },
+      },
    },
    theme: {
       extend: {
@@ -13,5 +38,5 @@ module.exports = {
       },
    },
    variants: {},
-   plugins: [require("@tailwindcss/ui")],
+   plugins: [require("@tailwindcss/ui"), require("@tailwindcss/typography")],
 };
--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,10 @@
 module.exports = {
+   parser: "sugarss",
    plugins: [
       require("postcss-import"),
       require("postcss-nested"),
       require("postcss-custom-properties"),
       require("tailwindcss")("./assets/css/tailwind.config.js"),
-      ...(process.env.NODE_ENV === "production"
-         ? [
-              require("autoprefixer"),
-              require("@fullhuman/postcss-purgecss")({
-                 content: ["./hugo_stats.json"],
-                 defaultExtractor: (content) => {
-                    let els = JSON.parse(content).htmlElements;
-                    const listado = els.tags.concat(els.classes, els.ids);
-                    return listado;
-                 },
-              }),
-           ]
-         : []),
+      ...(process.env.HUGO_ENVIROMENT === "production" ? [require("autoprefixer")] : []),
    ],
 };
