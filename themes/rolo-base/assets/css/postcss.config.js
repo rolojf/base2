a871eb2f35588e3ddd5b61228c32ec5bacba2596
rolojf <rolo@tutamail.com>
2020-10-22 12:06:52 -0500

    previo a convertir a hugo módulos lo de JS

--- themes/rolo-base/assets/css/postcss.config.js
+++ themes/rolo-base/assets/css/postcss.config.js
@@ -1,10 +1,8 @@
 module.exports = {
-   parser: "sugarss",
    plugins: [
-      require("postcss-import"),
       require("postcss-nested"),
       require("postcss-custom-properties"),
-      require("tailwindcss")("./assets/css/tailwind.config.js"),
+      require("tailwindcss")("themes/rolo-base/assets/css/tailwind.config.js"),
       ...(process.env.HUGO_ENVIROMENT === "production" ? [require("autoprefixer")] : []),
    ],
 };
--- themes/rolo-base/assets/js/SpecialElmApp.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var app = Elm.SpecialElmApp.init({
-   node: document.getElementById("myapp"),
-   flags: 6,
-});
-
-app.ports.toJs.subscribe((data) => {
-   console.log(data);
-});
